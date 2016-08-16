0ff472cfb56a58823eeceee646dba96ff412fe02
Andrey Azov <andrey.azov@gmail.com>
2016-08-16 11:53:56 +0300

    Hashtag composite decorator example

--- src/js/components/hashtag_autocompletion_field/index.jsx
+++ src/js/components/hashtag_autocompletion_field/index.jsx
@@ -1,24 +1,32 @@
 import React, {Component} from 'react';
-import {Editor, EditorState, Entity} from 'draft-js';
+import {Editor, EditorState, CompositeDecorator, Entity} from 'draft-js';
 
 export default class DescriptionField extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {editorState: EditorState.createEmpty()};
+        
+        const compositeDecorator = new CompositeDecorator([
+            {
+              strategy: hashtagStrategy,
+              component: HashtagSpan,
+            },
+          ]);
+        
+        this.state = {editorState: EditorState.createEmpty(compositeDecorator)};
         this.onChange = this.onChange.bind(this);
     }
 
     onChange(editorState) {
         // tag comment
-        let a = Entity;
+        // let a = Entity;
         this.setState({editorState});
             this.setState({content: editorState.getCurrentContent().getPlainText()})
 
 
         // get the current selection
        const selection = editorState.getSelection();
-       const anchorKey = selection.getAnchorKey();
+    //    const anchorKey = selection.getAnchorKey();
        const anchorOffset = selection.getAnchorOffset();
     //    console.log('anchor offset', anchorOffset);
 
@@ -46,5 +54,31 @@ export default class DescriptionField extends Component {
             </div>
         );
     }
-
+    
 }
+
+
+const styles = {
+  hashtag: {
+    color: 'rgba(95, 184, 138, 1.0)',
+  },
+};
+
+const HASHTAG_REGEX = /\#[\w\u0590-\u05ff]+/g;
+ 
+function hashtagStrategy(contentBlock, callback) {
+     findWithRegex(HASHTAG_REGEX, contentBlock, callback);
+}
+
+function findWithRegex(regex, contentBlock, callback) {
+    const text = contentBlock.getText();
+    let matchArr, start;
+    while ((matchArr = regex.exec(text)) !== null) {
+        start = matchArr.index;
+        callback(start, start + matchArr[0].length);
+    }
+}
+
+const HashtagSpan = (props) => {
+    return <span {...props} style={styles.hashtag}>{props.children}</span>;
+};
